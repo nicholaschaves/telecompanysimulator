2121360cae9d106f0fe191d95e144dda67acb1da
nicholaschaves <nicholaschaves2702@gmail.com>
2020-07-04 14:51:21 -0300

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -20,16 +21,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'portfolio-angular'`, () => {
+  it(`should have as title 'vizir-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('portfolio-angular');
+    expect(app.title).toEqual('vizir-project');
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to portfolio-angular!');
-  });
 });
